334f5b57a92c350c28934867ae80116c155714f8
Vyshak <vyshaknsc02@gmail.com>
2022-12-06 21:04:04 +0530

    Added attendance page

---
+++ src/pages/Attendance.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./components/Header";// 
+
+import Card from "./components/Card";
+
+const Second = () => {
+    return (
+        <div>
+            <Header />
+            <Card class="selection-title" title="Semester 1" />
+            <Card class="selection-title" title="Semester 2" />
+            <Card class="selection-title" title="Semester 3" />
+            <Card class="selection-title" title="Semester 4" />
+            <Card class="selection-title" title="Semester 5" />
+            <Card class="selection-title" title="Semester 6" />
+            <Card class="selection-title" title="Semester 7" />
+            <Card class="selection-title" title="Semester 8" />
+        </div>
+    );
+};
+
+export default Second;
